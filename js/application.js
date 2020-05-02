77547dc3b3870e5ad1b9e6830ad81e3d3774f8cf
Jason Ko <jekb2019@gmail.com>
2020-05-02 21:10:49 +1200

    Add right sidebar markup

--- js/application.js
+++ js/application.js
@@ -1,25 +1,26 @@
-const courses = document.querySelectorAll(".course")
-    for(const singleCourse of courses){
-        singleCourse.addEventListener("click",function(){
-            console.log(this.id);
-            switch(this.id){
-                case "course-1":
-                    location.href = "https://canvas.auckland.ac.nz/courses/33933";
-                    break;
-                case "course-2":
-                    location.href = "https://canvas.auckland.ac.nz/courses/46284";
-                    break;
-                case "course-3":
-                    location.href = "https://canvas.auckland.ac.nz/courses/47849";
-                    break;
-                case "course-4":
-                    location.href = "https://canvas.auckland.ac.nz/courses/47851";
-                    break;
-                case "course-5":
-                    location.href = "https://canvas.auckland.ac.nz/courses/47894";
-                    break;
-            }
-        });
-    }
+//Open actual Canvas webpage when a course rectangle is clicked
+const courses = document.querySelectorAll("#course-list .course")
+for(const singleCourse of courses){
+    singleCourse.addEventListener("click",function(){
+        console.log(this.id);
+        switch(this.id){
+            case "course-1":
+                location.href = "https://canvas.auckland.ac.nz/courses/33933";
+                break;
+            case "course-2":
+                location.href = "https://canvas.auckland.ac.nz/courses/46284";
+                break;
+            case "course-3":
+                location.href = "https://canvas.auckland.ac.nz/courses/47849";
+                break;
+            case "course-4":
+                location.href = "https://canvas.auckland.ac.nz/courses/47851";
+                break;
+            case "course-5":
+                location.href = "https://canvas.auckland.ac.nz/courses/47894";
+                break;
+        }
+    });
+}
 
 
